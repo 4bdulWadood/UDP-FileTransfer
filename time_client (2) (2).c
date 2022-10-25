79e758f0d3055aaa2d38f43d71d5a870c9c7dc24
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2022-10-25 16:06:00 -0400

    Add files via upload

---
+++ time_client (2) (2).c
@@ -0,0 +1,124 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define BUFSIZE 64;
+
+#define MSG "Any Message"
+
+int k = 1;
+
+int main(int argc, char **argv){
+
+while(1){
+	char *host = "localhost";
+	int port = 3000;
+
+	struct hostend *phe; /*Pointer to host information entry*/
+	struct sockaddr_in sin; /*An internet endpoint address*/
+	
+	struct pdu{
+	   char type;
+	   char data[100];
+	};		
+
+	int s, n, type; /*Socket Descriptor and Socket Type */
+
+	switch(argc){
+		case 1: 
+		    break;
+		case 2: 
+		    host = argv[1];
+	      case 3:
+		    host = argv[1];
+                port = atoi(argv[2]);
+		    break;
+		default:
+		    fprintf(stderr, "Usage: UDP time [Host [Port]]\n");
+		    exit(1);
+	}	
+
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+
+	/*Map host name to IP address, allowing for dotted decimal */
+	if(phe = gethostbyname(host)){
+	    memcpy(&sin.sin_addr, phe->h_addr, phe->h_length);
+	}
+	else if((sin.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE)
+		fprintf(stderr, "Can't get host entry \n");
+
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if(s < 0){
+		fprintf(stderr, "Can't create socket \n");
+	};
+
+	if(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+		fprintf(stderr, "Can't connect to %s %s \n", host, "Time");
+
+	struct pdu spdu;
+	spdu.type = 'C';
+	printf("Enter file name: \n");
+	fscanf(stdin, "%s", &spdu.data);
+	
+	write(s, &spdu, 101);
+
+	write_file(s);
+
+	printf("\nType 0 to exit the program or 1 to download another file");
+	int num;
+	scanf("%d", &num);
+	if(num==0){
+	     exit(0);
+	}
+}
+}
+
+void write_file(int s){
+	FILE *fp;
+
+	struct pdu {
+		char type;
+		char data[100];
+	}
+
+	char filename[16];
+	sprintf(filename, "file%d.txt", k);
+	k++;
+	int N;
+
+	while(1){
+		struct pdu spdu2;
+		char buffer[101];
+		
+		N = recv(s, buffer, 101, 0);
+		spdu2.type = buffer[0];
+		int b;
+		for(b = 0; b < sizeof(buffer)-1; b++){
+			buffer[b]  = buffer[b+1];
+		}
+
+		strcpy(spdu2.data, buffer);
+
+		if(spdu2.type == 'E'){
+			printf("File not found on server");
+			break;
+		}
+
+		if(spdu2.type == 'D' || spdu2.type == 'F'){
+			fp = fopen(filename, "a");
+			fprintf(fp, "%s", spdu2.data);
+			fflush(fp);
+		}
+
+		if(spdu2.type == 'F'){
+			printf("File Download Complete");
+			break;
+		}
+	}	
+}
---
+++ time_server (2) (2).c
@@ -0,0 +1,120 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <time.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]){
+	struct sockaddr_in fsin; /*The from address of the client */
+	char buf[101];	/*Input buffer; any size > 0 */
+	char *pts;
+	int sock;	
+	time_t now;
+	int alen;
+	struct sockaddr_in sin;
+	int s, type;
+	int port = 3000;
+	int n;
+
+	struct pdu {
+		char type;
+		char data[100];
+	};
+
+	switch(argc){
+		case 1: 
+			break;
+		case 2:
+			port = atoi(argv[1]);
+			break;	
+		default:
+			fprintf(stderr, "Usage: %s [port]\n", argv[0]);	
+			exit(1);
+	};
+
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(port);
+
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if(s < 0){
+		fprintf(stderr, "Can't create a socket \n");	
+	}
+
+	if(bind(s, (struct sockaddr *)&sin, sizeof(sin))<0){
+		fprintf(stderr, "can't bind to %d port \n", port);
+	}
+
+	while(1){
+		listen(s, 5);
+		alen = sizeof(fsin);
+		struct pdu spdu;	
+		int b;
+		if(recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&fsin, &alen) < 0)
+			fprintf(stderr, "recvfrom error \n");
+
+		spdu.type = buf[0];
+		
+		for(b = 0; b < sizeof(buf)-1; b++){
+			buf[b] = buf[b+1];
+		}
+
+		strcpy(spdu.data, buf);
+		
+		printf("%s\n", spdu.data);
+
+		bzero(buf,101);
+
+		FILE *fptr;
+
+		fptr = fopen(spdu.data, "r");
+	
+		char temp[100];
+		if(fptr == NULL){
+			char zeroarray[100] = {[0 ... 99] = 'X'};
+			struct pdu spdu2;
+			spdu2.type = 'E';
+			printf("Error Message Code: %c\n", spdu2.type);
+			strcpy(spdu2.data, zeroarray);
+			
+			if(sendto(s, &spdu2, 101, 0, (struct sockaddr *)&fsin, sizeof(fsin)) == -1){
+				printf("Error in Sending Data");
+				continue;
+			}
+			continue;
+		} 
+
+		struct stat buffer;
+
+		fptr = fopen(spdu.data, "r");
+		int fd = fileno(fptr);
+		fstat(fd, &buffer);
+		int size = buffer.st_size;
+		printf("%ld\n", size);
+			
+		while(fgets(temp, 100, fptr) != NULL){
+			struct pdu spdu3;
+			spdu3.type = 'D';
+			size = size - 100;
+			if(size<0){
+				spdu3.type = 'F';
+			}
+			
+			memcpy(spdu3.data, &temp[0], 99);
+
+			if(sendto(s, &spdu3, 101, 0, (struct sockaddr *)&fsin, sizeof(fsin)) == -1){
+				printf("Error in Sending Data");
+				continue;
+			}
+		}
+
+		bzero(temp, 100);
+		fflush(fptr);
+	}
+}
